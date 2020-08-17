3e1c8e4bbfd09cf2ed1c4251b911d22a896cb4e9
AndresBlandon47 <andresblandon1@gmail.com>
2020-08-17 09:15:53 -0500

    React commit

---
+++ Curso Practico de React/holamundo/src/components/HolaMundo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+const HolaMundo = () => (
+    <h1>Holissss</h1>
+)
+
+export default HolaMundo;
---
+++ Curso Practico de React/platziVideo/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Prueba from './components/Prueba';
+import './index.scss';
+
+ReactDOM.render(<Prueba />, document.getElementById('root'));
